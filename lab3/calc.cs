91c010fa4216aafcb6f005ddc75d10ffbf3f21cc
shivam4490 <68771796+shivam4490@users.noreply.github.com>
2020-08-09 16:07:53 +0530

    Add files via upload

--- lab3/calc.cs
+++ lab3/calc.cs
@@ -1,14 +1,19 @@
 using System;
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.0.0")]
 namespace math
 {
     public class calc
     {
         public int multiplication(int a,int b)
 		{
+			Console.WriteLine("In a new Vesrion of dll");
 			return a*b;
 		}
 		public int addition(int a,int b)
 		{
+			Console.WriteLine("In a new Vesrion of dll");
 			return a+b;
 		}
     }
