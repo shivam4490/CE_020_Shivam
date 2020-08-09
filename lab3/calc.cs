1ae65b9aa8addcf5acb761a6163f24b45187e639
shivam4490 <68771796+shivam4490@users.noreply.github.com>
2020-08-09 16:05:56 +0530

    Add files via upload

---
+++ lab3/calc.cs
@@ -0,0 +1,15 @@
+using System;
+namespace math
+{
+    public class calc
+    {
+        public int multiplication(int a,int b)
+		{
+			return a*b;
+		}
+		public int addition(int a,int b)
+		{
+			return a+b;
+		}
+    }
+}
